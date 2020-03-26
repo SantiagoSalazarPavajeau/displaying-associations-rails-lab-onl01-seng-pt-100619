767bc4382ebf0c47588d461d63cb357a3c1d5923
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-03-26 21:30:18 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist = Artist.create(name: "Daft Punk")
+atw = Song.create(artist: artist, title: "atw")
+sau = Song.create(artist: artist, title: "sau")
+
